474aa092a0158eb2617d4ae0d2d47aa9e294511a
eimhincc <eimhincc@gmail.com>
2021-12-19 11:23:38 +0000

    fetch script update

--- script2.py
+++ script2.py
@@ -7,16 +7,28 @@ print("Demonstration python based mongodb access");
 
 import pymongo              # for mongodb access
 import pprint               # for pretty printing db data
-
+4
 #Let's get the user object from the db
 
 # Establish connection
-conn = "mongodb://localhost:27017"
-client = pymongo.MongoClient(conn)
 
-# Create a database
-db = client.classDB
+client = pymongo.MongoClient("mongodb+srv://Test:Password1@cluster0.bg843.mongodb.net/test?retryWrites=true&w=majority")
+db = client.test
 
-for user in db.githubuser.find({'location': {'$exists': True}}):
-    pprint.pprint(user)
-    print()
+
+with open('data.csv', 'w') as f:
+    f.write('User,RepoCount\n')
+    dct = db.githubuser.find({'user': {'$exists': True}})
+    for user in dct:
+        pprint.pprint(user)
+        print()
+        f.write(user['user'] + ',' + str(user['public_repos']) + '\n')
+        
+with open('data2.csv', 'w') as f:
+    f.write('group, Watchers, LinesPerFork, AvrSize\n')
+    dct = db.githubuser.find({'user': {'$exists': True}})
+    for user in dct:
+        pprint.pprint(user)
+        print()
+        f.write(user['user'] + ',' + str(user['total_watchers']) + ',' + str(user['linesPerFork'])+ ',' + str(user['avrSize']) + '\n')
+
